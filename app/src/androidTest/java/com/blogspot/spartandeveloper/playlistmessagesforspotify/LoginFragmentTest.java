39c1098d3681d73a73d7a98047ebaac20ae28918
Trevor <trevornemanic@gmail.com>
2017-12-30 13:02:56 -0800

    create LoginFragment MVP create failing UI test for LoginFragment add deps for fragment espresso testing

---
+++ app/src/androidTest/java/com/blogspot/spartandeveloper/playlistmessagesforspotify/LoginFragmentTest.java
@@ -0,0 +1,58 @@
+package com.blogspot.spartandeveloper.playlistmessagesforspotify;
+
+import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.intent.rule.IntentsTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import com.android21buttons.fragmenttestrule.FragmentTestRule;
+import com.blogspot.spartandeveloper.playlistmessagesforspotify.test.common.TestComponentRule;
+import com.blogspot.spartandeveloper.playlistmessagesforspotify.ui.login.LoginFragment;
+import com.blogspot.spartandeveloper.playlistmessagesforspotify.ui.main.MainActivity;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.RuleChain;
+import org.junit.rules.TestRule;
+import org.junit.runner.RunWith;import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static android.support.test.espresso.matcher.RootMatchers.withDecorView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+
+
+@RunWith(AndroidJUnit4.class)
+public class LoginFragmentTest {
+
+    public final TestComponentRule component =
+            new TestComponentRule(InstrumentationRegistry.getTargetContext());
+
+    @Rule
+    public FragmentTestRule<?, LoginFragment> fragmentTestRule =
+            FragmentTestRule.create(LoginFragment.class);
+
+    // allows mocking of intents
+    @Rule
+    public IntentsTestRule<MainActivity> intentsTestRule =
+            new IntentsTestRule<>(MainActivity.class);
+
+    // TestComponentRule needs to go first to make sure the Dagger ApplicationTestComponent is set
+    // in the Application before any Activity is launched.
+    @Rule
+    public final TestRule chain = RuleChain.outerRule(component).around(fragmentTestRule);
+
+
+    @Test
+    public void whenLoginFailed_thenShowFailureToast() {
+
+
+
+
+        onView(withText(R.string.login_failed))
+                .inRoot(withDecorView(not(is(activity.getWindow().getDecorView()))))
+                .check(matches(isDisplayed()));
+
+    }
+
+
+
+}
---
+++ app/src/main/java/com/blogspot/spartandeveloper/playlistmessagesforspotify/ui/login/LoginPresenter.java
@@ -0,0 +1,9 @@
+package com.blogspot.spartandeveloper.playlistmessagesforspotify.ui.login;
+
+import com.blogspot.spartandeveloper.playlistmessagesforspotify.ui.base.BasePresenter;
+
+public class LoginPresenter extends BasePresenter<LoginMvpView> {
+
+
+
+}
