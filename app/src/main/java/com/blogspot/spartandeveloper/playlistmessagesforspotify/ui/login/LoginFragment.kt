0a07a8da22e30e2b839c967861ce97cf9369f04d
Trevor <trevornemanic@gmail.com>
2018-03-20 18:17:03 -0700

    fix bug where spotify login btn click did not work (after upgrade to Kotlin)

--- app/src/main/java/com/blogspot/spartandeveloper/playlistmessagesforspotify/ui/login/LoginFragment.kt
+++ app/src/main/java/com/blogspot/spartandeveloper/playlistmessagesforspotify/ui/login/LoginFragment.kt
@@ -5,32 +5,32 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import butterknife.ButterKnife
-import butterknife.OnClick
 import com.blogspot.spartandeveloper.playlistmessagesforspotify.R
 import com.spotify.sdk.android.authentication.AuthenticationClient
 import com.spotify.sdk.android.authentication.AuthenticationRequest
 import com.spotify.sdk.android.authentication.AuthenticationResponse
+import kotlinx.android.synthetic.main.fragment_login.*
+import kotlinx.android.synthetic.main.fragment_login.view.*
+
 
 class LoginFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_login, container, false)
-        ButterKnife.bind(this, view)
-        return view
-    }
 
-    @OnClick(R.id.btn_spotify_login)
-    internal fun onSpotifyLoginButtonClicked() {
-        val CLIENT_ID = getString(R.string.spotify_client_id)
-        val REDIRECT_URI = "playlistmessagesforspotify://callback"
-        val builder = AuthenticationRequest.Builder(
-                CLIENT_ID, AuthenticationResponse.Type.TOKEN, REDIRECT_URI)
-        val scopes = arrayOf("playlist-read-private", "playlist-modify-public", "playlist-modify-private", "playlist-read-collaborative")
-        builder.setScopes(scopes)
-        val request = builder.build()
-        AuthenticationClient.openLoginInBrowser(activity, request)
+        view.spotifyLoginBtn.setOnClickListener {
+            val CLIENT_ID = getString(R.string.spotify_client_id)
+            val REDIRECT_URI = "playlistmessagesforspotify://callback"
+            val builder = AuthenticationRequest.Builder(
+                    CLIENT_ID, AuthenticationResponse.Type.TOKEN, REDIRECT_URI)
+            val scopes = arrayOf("playlist-read-private", "playlist-modify-public", "playlist-modify-private", "playlist-read-collaborative")
+            builder.setScopes(scopes)
+            val request = builder.build()
+            AuthenticationClient.openLoginInBrowser(activity, request)
+        }
+
+        return view
     }
 
     companion object {
